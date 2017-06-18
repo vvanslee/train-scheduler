d67c13f69da29542d039314be9d1c4b8045d9965
vvanslee <vvanslee@gmail.com>
2017-06-18 02:51:37 -0500

    working firebase

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,50 +1,69 @@
 
+var config = {
+	apiKey: "AIzaSyCdYKeMfARf5TKwlOAoZczbSEho0rJ9qI4",
+	authDomain: "train-scheduler-cd7fb.firebaseapp.com",
+	databaseURL: "https://train-scheduler-cd7fb.firebaseio.com",
+	projectId: "train-scheduler-cd7fb",
+	storageBucket: "train-scheduler-cd7fb.appspot.com",
+	messagingSenderId: "210940462057"
+};
+firebase.initializeApp(config);
+
+var database = firebase.database();
+
 $(document).on("click", "#addTrainBtn", function() {
 
 	var trainName = $("#inputName").val();
-	var destination = $("#inputDestination").val();
+	var trainDest = $("#inputDestination").val();
 	var firstTime = $("#inputFirstTime").val();
-	var frequency = $("#inputFrequency").val();
+	var frequency = parseInt( $("#inputFrequency").val() );
 
-	console.log(trainName);
-	console.log(destination);
-	console.log(firstTime);
-	console.log(frequency);
-
-	if (trainName === "" || destination === "" || firstTime === "" || frequency === "") {
+	if (trainName === "" || trainDest === "" || firstTime === "" || frequency === "") {
 		alert("Please fill out all fields");
 	} else {
 
-		// var arrival = ...;
-		// var minAway = ...;
-		// console.log(arrival);
-		// console.log(minAway);
-
-		var newRow = $("<div class='row' id='trainSched'>");
-		$("#schedContainer").append(newRow);
-
-		var newDiv1 = $("<div class='col-xs-3'>");
-		var newDiv2 = $("<div class='col-xs-3'>");
-		var newDiv3 = $("<div class='col-xs-2'>");
-		// var newDiv4 = $("<div class='col-xs-2'>");
-		// var newDiv5 = $("<div class='col-xs-2'>");
-
-		newDiv1.append(trainName);
-		newDiv2.append(destination);
-		newDiv3.append(frequency);
-		// newDiv4.append(arrival);
-		// newDiv5.append(minAway);
-
-		$("#trainSched").append(newDiv1);
-		$("#trainSched").append(newDiv2);
-		$("#trainSched").append(newDiv3);
-		// $("#trainSched").append(newDiv4);
-		// $("#trainSched").append(newDiv5);
+		database.ref().push({
+			name: trainName,
+			destination: trainDest,
+			first: firstTime,
+			freq: frequency
+		});
 
 		$("#inputName").val("");
 		$("#inputDestination").val("");
 		$("#inputFirstTime").val("");
 		$("#inputFrequency").val("");
 	}
-
 });
+
+database.ref().on("child_added", function(childSnapshot){
+
+	var arrival = "n/a";
+	var minAway = "n/a";
+	console.log(arrival);
+	console.log(minAway);
+
+	var newRow = $("<div class='row' id='trainSched'>");
+	$("#schedContainer").append(newRow);
+
+	var newDiv1 = $("<div class='col-xs-3'>");
+	var newDiv2 = $("<div class='col-xs-3'>");
+	var newDiv3 = $("<div class='col-xs-2'>");
+	var newDiv4 = $("<div class='col-xs-2'>");
+	var newDiv5 = $("<div class='col-xs-2'>");
+
+	newDiv1.append(childSnapshot.val().name);
+	newDiv2.append(childSnapshot.val().destination);
+	newDiv3.append(childSnapshot.val().freq);
+	newDiv4.append(arrival);
+	newDiv5.append(minAway);
+
+	$("#trainSched").append(newDiv1);
+	$("#trainSched").append(newDiv2);
+	$("#trainSched").append(newDiv3);
+	$("#trainSched").append(newDiv4);
+	$("#trainSched").append(newDiv5);
+
+}, function(errorObject) {
+	console.log("Errors Handled: " + errorObject.code);
+}); 
