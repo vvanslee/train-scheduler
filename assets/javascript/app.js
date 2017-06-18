1910e69583316fbef066d6c6285e606544e1227a
vvanslee <vvanslee@gmail.com>
2017-06-18 01:27:55 -0500

    updated js

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -11,27 +11,40 @@ $(document).on("click", "#addTrainBtn", function() {
 	console.log(firstTime);
 	console.log(frequency);
 
-	// var arrival = ...;
-	// var minAway = ...;
-	// console.log(arrival);
-	// console.log(minAway);
+	if (trainName === "" || destination === "" || firstTime === "" || frequency === "") {
+		alert("Please fill out all fields");
+	} else {
 
-	var newDiv1 = $("<div class='col-xs-3'>");
-	var newDiv2 = $("<div class='col-xs-3'>");
-	var newDiv3 = $("<div class='col-xs-2'>");
-	// var newDiv4 = $("<div class='col-xs-2'>");
-	// var newDiv5 = $("<div class='col-xs-2'>");
+		// var arrival = ...;
+		// var minAway = ...;
+		// console.log(arrival);
+		// console.log(minAway);
 
-	newDiv1.append(trainName);
-	newDiv2.append(destination);
-	newDiv3.append(frequency);
-	// newDiv4.append(arrival);
-	// newDiv5.append(minAway);
+		var newRow = $("<div class='row' id='trainSched'>");
+		$("#schedContainer").append(newRow);
 
-	$("#trainSched").append(newDiv1);
-	$("#trainSched").append(newDiv2);
-	$("#trainSched").append(newDiv3);
-	// $("#trainSched").append(newDiv4);
-	// $("#trainSched").append(newDiv5);
+		var newDiv1 = $("<div class='col-xs-3'>");
+		var newDiv2 = $("<div class='col-xs-3'>");
+		var newDiv3 = $("<div class='col-xs-2'>");
+		// var newDiv4 = $("<div class='col-xs-2'>");
+		// var newDiv5 = $("<div class='col-xs-2'>");
+
+		newDiv1.append(trainName);
+		newDiv2.append(destination);
+		newDiv3.append(frequency);
+		// newDiv4.append(arrival);
+		// newDiv5.append(minAway);
+
+		$("#trainSched").append(newDiv1);
+		$("#trainSched").append(newDiv2);
+		$("#trainSched").append(newDiv3);
+		// $("#trainSched").append(newDiv4);
+		// $("#trainSched").append(newDiv5);
+
+		$("#inputName").val("");
+		$("#inputDestination").val("");
+		$("#inputFirstTime").val("");
+		$("#inputFrequency").val("");
+	}
 
 });
