1ac7e59b66cf9c8b02d2904b780412fb748a9c80
vvanslee <vvanslee@gmail.com>
2017-06-18 01:02:26 -0500

    initial js

---
+++ assets/javascript/app.js
@@ -0,0 +1,37 @@
+
+$(document).on("click", "#addTrainBtn", function() {
+
+	var trainName = $("#inputName").val();
+	var destination = $("#inputDestination").val();
+	var firstTime = $("#inputFirstTime").val();
+	var frequency = $("#inputFrequency").val();
+
+	console.log(trainName);
+	console.log(destination);
+	console.log(firstTime);
+	console.log(frequency);
+
+	// var arrival = ...;
+	// var minAway = ...;
+	// console.log(arrival);
+	// console.log(minAway);
+
+	var newDiv1 = $("<div class='col-xs-3'>");
+	var newDiv2 = $("<div class='col-xs-3'>");
+	var newDiv3 = $("<div class='col-xs-2'>");
+	// var newDiv4 = $("<div class='col-xs-2'>");
+	// var newDiv5 = $("<div class='col-xs-2'>");
+
+	newDiv1.append(trainName);
+	newDiv2.append(destination);
+	newDiv3.append(frequency);
+	// newDiv4.append(arrival);
+	// newDiv5.append(minAway);
+
+	$("#trainSched").append(newDiv1);
+	$("#trainSched").append(newDiv2);
+	$("#trainSched").append(newDiv3);
+	// $("#trainSched").append(newDiv4);
+	// $("#trainSched").append(newDiv5);
+
+});
